aa9aab31830f806eddf48e5c52eaad17e7075807
Glitch (dramatic-lead) <none>
2017-10-30 16:10:41 +0000

    Checkpoint

--- public/client.js
+++ public/client.js
@@ -3,13 +3,16 @@
 $(function() {  
  
  $('input').on('input',()=>{$('.shortenedUrl').css('visibility','hidden');}) 
+ $('button.copy').click((event)=>{
+   let url = $('section>pre').text();
+   document.execCommand('copy', false, url||'');
+ });
   
  $('form').submit(function(event) {
     event.preventDefault();
     var url = $('input').val();
     $.post('/urlparser',{url:url}, function(shorten,status) {
       if(status!=='success') console.error(status);
-      console.log(shorten);
       $('section>pre').text(shorten);
       $('.shortenedUrl').css('visibility','visible');
       $('input').val('');
